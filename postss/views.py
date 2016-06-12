5ee1586025d1dcce65dea48f59fe98c983cad3ba
rakeshgunduka <rakesh.gunduka@gmail.com>
2016-06-12 23:52:03 +0530

    change

--- postss/views.py
+++ postss/views.py
@@ -33,7 +33,7 @@ def post_list(request):
 		queryset = paginator.page(page)
 	except PageNotAnInteger:
 		# If page is not an integer, deliver first page.
-		queryset = paginator.page(1)
+		pass#queryset = paginator.page(1)
 	except EmptyPage:
 		# If page is out of range (e.g. 9999), deliver last page of results.
 		queryset = paginator.page(paginator.num_pages)
